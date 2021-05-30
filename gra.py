a3519540ebbebb40e43877b6287c64fa8b76640b
BognaPrzy <83338881+BognaPrzy@users.noreply.github.com>
2021-05-31 01:06:36 +0200

    Update gra.py
    Znikają owoce i cukierki, gdy nastąpi kolizja z wózkiem oraz naliczają się punkty (jest chwilowy problem, bo się też zerują z każdą iteracją)
    Powiększony ekran, bo pracuję nad tym, żeby pojawiały się z boku owoce i cukierki, które właśnie się zebrało

--- gra.py
+++ gra.py
@@ -1,11 +1,10 @@
 import pygame
 import random
-import math
 
 pygame.init()
 pygame.display.set_caption("Cupola")
 
-screen_width = 800
+screen_width = 900
 screen_height = 500
 #boki ekranu gry - czyli 500-50
 margin = 50
@@ -13,15 +12,15 @@ margin = 50
 screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.Font(pygame.font.get_default_font(), 30)
 
-background = pygame.image.load('lidl.jpg')
+background = pygame.image.load('/Users/bogna/Desktop/programowanie/gra/lidl.jpg')
 
 
 #klasy player
 class Player:
     def __init__(self):
-        self.image = pygame.image.load('trolley.png')
+        self.image = pygame.image.load('/Users/bogna/Desktop/programowanie/gra/trolley.png')
         #obecna lokalizacja wozka i to ze pojawia sie na srodku na poczatku gry
-        self.x = screen_width/2
+        self.x = (screen_width-100)/2
         self.y = screen_height-2*margin
         #predkosc z jaka porusza sie wozek
         self.speed = 0.5
@@ -41,8 +40,8 @@ class Player:
 #utworzony gracz z klasy
 active_player = Player()
 
-fruit_paths = ['apple.png', 'banana.png']
-sweets_paths = ['candy1.png', 'candy2.png']
+fruit_paths = ['/Users/bogna/Desktop/programowanie/gra/apple.png', '/Users/bogna/Desktop/programowanie/gra/banana.png']
+sweets_paths = ['/Users/bogna/Desktop/programowanie/gra/candy1.png', '/Users/bogna/Desktop/programowanie/gra/candy2.png']
 
 
 #lista wgranych obrazkow
@@ -58,7 +57,7 @@ class MovingObject:
         self.path = random.choice(fruit_paths + sweets_paths)
         # zwraca nam np. apple
         self.type = self.path.split("/")[-1][0:-4]
-        self.x = random.randint(0+margin, screen_width-margin)
+        self.x = random.randint(0+margin, (screen_width-100)-margin)
         self.y = margin
         self.speed = 5
 
@@ -66,12 +65,23 @@ class MovingObject:
             if image[0] == self.path:
                 self.img = image[1]
 
+    def x(self):
+        return self.x
 
+    def y():
+        return self.y
 
+object = MovingObject()
 
-
-
-
+#sprawdzanie kolizji obiektu z wózkiem
+'''def Collision(object_x,object_y,trolley_x,trolley_y):
+    distance = ((object_x - trolley_x)**2 +
+    (object_y - trolley_y)**2)**1/2
+    if distance <= 550:
+        return True
+    else:
+        return False'''
+    
 
 #lista spadających cukierków i owoców, pierwszy spada owoc
 objects = []
@@ -108,8 +118,8 @@ while running:
     #wozek nie wyjezdza poza ekran
     if active_player.x <= 0:
         active_player.x = 0
-    elif active_player.x >= screen_width-2*margin:
-        active_player.x = screen_width-2*margin
+    elif active_player.x >= (screen_width-100)-2*margin:
+        active_player.x = (screen_width-100)-2*margin
 
     # przesuniecie wozka
     screen.blit(active_player.image, (active_player.x,active_player.y))
@@ -127,6 +137,9 @@ while running:
 
     #obiekty, ktore sa poza oknem gry
     lost = []
+    chaught_objects = []
+    #nie mogą być tak zdefinionwe punkty, bo je ciągle zeruje
+    points = 0
 
     #renderuje owoce i cukierki
     for i, object in enumerate(objects):
@@ -138,12 +151,31 @@ while running:
         if object.y >= screen_height:
             lost.append(i)
 
+        #sprawdza, czy następuje kolizja i usuwa złapany obiekt
+        #(nwm czemu to samo jako def nie działa)
+        distance = ((object.x - active_player.x)**2 + (object.y - active_player.y)**2)**1/2
+        if distance <= 550:
+            lost.append(i)
+            chaught_objects.append(i)
+            #dodaje lub odejmuje punkty odpowiednio dla typu
+            if object.type == "apple" or object.type == "banana":
+                points += 1
+            else:
+                points += -1
+        
+
     # usuwa owoce i cukierki, ktore sa poza ekranem z listy
     new_objects = []
     for i, object in enumerate(objects):
           if i not in lost:
               new_objects.append(object)
     objects = new_objects
+
+
+    '''if object in chaught_objects:
+        screen.blit(object.img, (830, 50))'''
+
+    #wyświetla złapane obiekty z boku
 
     # zapobiega dalszemu przesuwaniu sie owocow i cukierkow
     move_object = False
