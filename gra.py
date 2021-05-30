d030f5f4f322f697fe0585ae68613f9697f70e7a
23Xader <33560596+23Xader@users.noreply.github.com>
2021-05-30 16:17:21 +0200

    Update gra.py
    Całkowita zmiana na klasy. Należy zmienić ścieżki.

--- gra.py
+++ gra.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 pygame.display.set_caption("Cupola")
@@ -12,34 +13,68 @@ margin = 50
 screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.Font(pygame.font.get_default_font(), 30)
 
-background = pygame.image.load('/Users/marta/Desktop/gra/lidl.jpg')
+background = pygame.image.load('lidl.jpg')
 
-#słownik można zmenić na klasy
-player = {
-        "image": pygame.image.load('/Users/marta/Desktop/gra/trolley.png'),
+
+#klasy player
+class Player:
+    def __init__(self):
+        self.image = pygame.image.load('trolley.png')
         #obecna lokalizacja wozka i to ze pojawia sie na srodku na poczatku gry
-        "x": screen_width/2,
-        "y": screen_height-2*margin,
+        self.x = screen_width/2
+        self.y = screen_height-2*margin
         #predkosc z jaka porusza sie wozek
-        "speed": 0.5
-}
+        self.speed = 0.5
+
+    #definicja do zmieniania wartości x playera
+    def place_x(self, change):
+        self.x += change
+
+    #definicja do zmieniania wartości y playera
+    def place_y(self, change):
+        self.y += change
+
+    #definicja do zmieniania wartości speed playera
+    def speed_change(self, change):
+        self.speed += change
+
+#utworzony gracz z klasy
+active_player = Player()
+
+fruit_paths = ['apple.png', 'banana.png']
+sweets_paths = ['candy1.png', 'candy2.png']
+
 
 #lista wgranych obrazkow
-fruit_imgs = [pygame.image.load(image) for image in ['/Users/marta/Desktop/gra/apple.png', '/Users/marta/Desktop/gra/banana.png']]
-sweets_imgs = [pygame.image.load(image) for image in ['/Users/marta/Desktop/gra/candy1.png', '/Users/marta/Desktop/gra/candy2.png']]
+fruit_imgs = [[path, pygame.image.load(path)] for path in fruit_paths]
+sweets_imgs = [[path, pygame.image.load(path)] for path in sweets_paths]
 
-#zwraca owoc albo cukierek
-def get_object(type="fruit"):
-    #lokalizacja owockow i cukierkow
-    return {"image" : random.choice(fruit_imgs if type=="fruit" else sweets_imgs),
-            "x" : random.randint(0+margin, screen_width-margin),
-            "y" : margin,
-            "speed" : 5,
-            #liczy punkty, 1=owoc, -1=cukierek
-            "points": 1 if type=="fruit" else -1}
+
+class MovingObject:
+
+    def __init__(self):
+        # wylosować obrazek
+        # rozpoznać typ
+        self.path = random.choice(fruit_paths + sweets_paths)
+        # zwraca nam np. apple
+        self.type = self.path.split("/")[-1][0:-4]
+        self.x = random.randint(0+margin, screen_width-margin)
+        self.y = margin
+        self.speed = 5
+
+        for image in fruit_imgs+sweets_imgs:
+            if image[0] == self.path:
+                self.img = image[1]
+
+
+
+
+
+
+
 
 #lista spadających cukierków i owoców, pierwszy spada owoc
-objects = [get_object("fruit")]
+objects = []
 
 new_object_time = 0
 move_object_time = 0
@@ -53,35 +88,36 @@ while running:
     screen.fill((0, 0, 0))
 
     screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         #w jaki sposob wozek ma sie ruszac
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_movement = -player["speed"]
+                player_movement = -active_player.speed
             if event.key == pygame.K_RIGHT:
-                player_movement = player["speed"]
+                player_movement = active_player.speed
         if event.type == pygame.KEYUP:
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
                 player_movement = 0
 
     # aktualizacja polozenia wozka w poziomie
-    player["x"] += player_movement
+    active_player.place_x(player_movement)
 
     #wozek nie wyjezdza poza ekran
-    if player["x"] <= 0:
-        player["x"] = 0
-    elif player["x"] >= screen_width-2*margin:
-        player["x"] = screen_width-2*margin
+    if active_player.x <= 0:
+        active_player.x = 0
+    elif active_player.x >= screen_width-2*margin:
+        active_player.x = screen_width-2*margin
 
     # przesuniecie wozka
-    screen.blit(player["image"], (player["x"], player["y"]))
+    screen.blit(active_player.image, (active_player.x,active_player.y))
 
     # losowosc renderowania owocow i cukierkow
     if new_object_time == 750:
         type = bool(random.getrandbits(1))
-        objects.append(get_object("fruit" if type else "sweet"))
+        objects.append(MovingObject())
         new_object_time = 0
 
     # co ile spadaja owoce i cukierki
@@ -96,10 +132,10 @@ while running:
     for i, object in enumerate(objects):
         # przesuwa owoce i cukierki
         if move_object:
-            object["y"] += object["speed"]
-        screen.blit(object["image"], (object["x"], object["y"]))
+            object.y += object.speed
+        screen.blit(object.img, (object.x, object.y))
         # rejestruje obiekty poza oknem gry
-        if object["y"] >= screen_height:
+        if object.y >= screen_height:
             lost.append(i)
 
     # usuwa owoce i cukierki, ktore sa poza ekranem z listy
@@ -111,6 +147,7 @@ while running:
 
     # zapobiega dalszemu przesuwaniu sie owocow i cukierkow
     move_object = False
+
 
     # inkrementacja czasu liczników
     new_object_time += 1
